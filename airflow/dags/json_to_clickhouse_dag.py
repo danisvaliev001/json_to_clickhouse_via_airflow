883869a194f79122e46c90ae2e92d9b1f7b957a8
danisvaliev001 <danisvaliev001@gmail.com>
2020-04-27 06:42:42 +0300

    removed old configuration

--- airflow/dags/json_to_clickhouse_dag.py
+++ airflow/dags/json_to_clickhouse_dag.py
@@ -1,7 +1,6 @@
 from airflow import DAG
 from airflow.hooks.clickhouse_hook import ClickHouseHook
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
 import os
 
